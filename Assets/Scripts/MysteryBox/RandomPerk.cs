4da18e13a3b247d2d8fb67ed9eccc1997b085f35
Álvaro García <bytanquetor19@gmail.com>
2026-05-07 12:40:31 +0200

    CREATE: Box Tp,SuperStar and Intangible Perks

--- Assets/Scripts/MysteryBox/RandomPerk.cs
+++ Assets/Scripts/MysteryBox/RandomPerk.cs
@@ -7,8 +7,8 @@ using Random = UnityEngine.Random;
 
 public class RandomPerk : MonoBehaviour
 {
-    private String[] abilities = {"Under2Hits","Speed","Under5s"};
-    
+    private String[] abilities = {"SuperHit","Intangible","Freeze","Teleport","SuperStar","MagneticField"};
+    [SerializeField] private Transform holePosition;
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Player")
@@ -18,23 +18,72 @@ public class RandomPerk : MonoBehaviour
             string randomAbility = abilities[randomIndex];
             switch (randomAbility)
             {
-                case "Under2Hits":
+                case "SuperHit":
                     //Activar buff
-                    //other.gameObject.GetComponent<Player>().Under2Hits;
+                    
                     break;
 
-                case "Speed":
+                case "Intangible":
                    //Activar buff
-                   //other.gameObject.GetComponent<Player>().Speed;
+                    StartCoroutine(Intangible(other.gameObject));
                     break;
 
-                case "Under5s":
+                case "Freeze":
                     //Activar buff
                     //other.gameObject.GetComponent<Player>().Under5s;
                     break;
+                case "Teleport":
+                    //Activar buff
+                    other.gameObject.transform.position = holePosition.position;
+                    other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                    break;
+                case "SuperStar":
+                    //Activar buff
+                    StartCoroutine(SuperStar(other.gameObject));
+                    break;
+                case "MagneticField":
+                    //Activar buff
+                    //other.gameObject.GetComponent<Player>().Under5s;
+                    break;
+                
                 
             }
             Destroy(this.gameObject);
         }
     }
+    
+    private IEnumerator Intangible(GameObject gameObject)
+    {
+        if (gameObject.GetComponent<SphereCollider>() != null)
+        {
+            gameObject.GetComponent<SphereCollider>().isTrigger = true;
+            yield return new WaitForSeconds(8f);
+            gameObject.GetComponent<SphereCollider>().isTrigger = false;
+        }
+    }
+
+    private IEnumerator SuperStar(GameObject gameObject)
+    {
+        if (gameObject.GetComponent<MeshRenderer>() != null)
+        {
+            Color originalColor = gameObject.GetComponent<MeshRenderer>().material.color;
+            int buffTime = 0;
+            while (buffTime <= 8)
+            {
+                gameObject.GetComponent<MeshRenderer>().material.color = new Color(
+                    Random.value,
+                    Random.value,
+                    Random.value
+                );
+                yield return new WaitForSeconds(1f);
+                buffTime += 1;
+
+            }
+
+            gameObject.GetComponent<MeshRenderer>().material.color = originalColor;
+            buffTime = 0;
+        }
+        yield return null;
+    }
 }
+
